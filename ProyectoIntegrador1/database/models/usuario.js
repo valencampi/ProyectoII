e8a56708416dc85a54c6c2ce832f9af24f8222c0
maiteGalindez <galindezmaite@gmail.com>
2020-05-15 16:28:23 -0300

    erros dataTypes

--- ProyectoIntegrador1/database/models/usuario.js
+++ ProyectoIntegrador1/database/models/usuario.js
@@ -3,16 +3,16 @@ module.exports= function(squelize, DataTypes) {
         
         'Usuarios',
         { 
-          nombre: DataType.STRING,
-          email: DataType.STRING,
-          pasword: DataType.STRING,
-          edad: DataType.DECIMAl,
-          Fecha_de_nacimiento: DataType.DECIAML, //aca node que va
+          nombre: DataTypes.STRING,
+          email: DataTypes.STRING,
+          pasword: DataTypes.STRING,
+          edad: DataTypes.DECIMAl,
+          Fecha_de_nacimiento: DataTypes.DECIAML, //aca node que va
         },
 
         {
             TableName:'usuario', 
-            timestamps:false,
+            timestamps: false,
         }
     );
     return usuario;
