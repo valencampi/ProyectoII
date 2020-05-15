6ca71e1b91b5fe20bb91e66afa42321055ed9831
Marcos <mestrada@udesa.edu.ar>
2020-05-14 21:34:30 -0300

    p

--- ProyectoIntegrador1/database/config/config.js
+++ ProyectoIntegrador1/database/config/config.js
@@ -3,7 +3,7 @@ module.exports=
   "development": {
     "username": "root",
     "password": null,
-    "database": "usuarios",
+    "database": "usuarioos",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": false
