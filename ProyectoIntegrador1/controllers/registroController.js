7788947b461927412ace1576baf2e52c56743766
Valentina <vcampitelli@udesa.edu.ar>
2020-06-01 16:15:05 -0300

    Contrasena encriptada

--- ProyectoIntegrador1/controllers/registroController.js
+++ ProyectoIntegrador1/controllers/registroController.js
@@ -1,12 +1,14 @@
 const DB = require('../database/models');
 const OP = DB.Sequelize.Op;
+const bcrypt = require('bcryptjs');
 
 module.exports = {
     index: function (req, res){
         res.render('registro')
        },
     
-    registrado: (req, res) =>{     
+    registrado: (req, res) =>{    
+        req.body.password = bcrypt.hashSync(req.body.password, 10) 
         DB.Usuarios.create(req.body)
         .then(function(index){
             return res.redirect('/')
