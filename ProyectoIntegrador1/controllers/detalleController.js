071615b80c6b791013c78d9a8d23ac36fe8853c4
Valentina <vcampitelli@udesa.edu.ar>
2020-06-07 20:38:44 -0300

    Estilo y detalle

--- ProyectoIntegrador1/controllers/detalleController.js
+++ ProyectoIntegrador1/controllers/detalleController.js
@@ -1,12 +1,20 @@
-
 const DB = require('../database/models');
 const OP = DB.Sequelize.Op;
-// let bcryptjs = require ('bcryptjs');
+// const bcrypt = require('bcryptjs');
+const fetch = require('node-fetch')
 
 module.exports = {
-    index: function (req, res) {
-        res.send('  ')
-    },
+    index: function (req, res){
+        fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=6e11caa7480bb2cb18a5bff7908d4f53&language=en-US")
+            .then(function(respond) {
+                return respond.json()
+            })
+          .then(function(data) {
+             var arrayGeneros = data.genres
+             res.render('', {arrayGeneros})
+        })
+       },
+
    detalle:  function(req, res) {
        //return res.send("el id de usuario es " + req.params.id)
        DB.Usuarios.findByPk(req.params.id, {
