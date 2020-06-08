2a7dbe457f23b0a7354e02dc4ed247160f2d5d5d
Valentina <vcampitelli@udesa.edu.ar>
2020-06-08 17:32:30 -0300

    Validacion contrasena resena

--- ProyectoIntegrador1/controllers/resenaController.js
+++ ProyectoIntegrador1/controllers/resenaController.js
@@ -10,21 +10,19 @@ module.exports = {
     //crear una resena
     resena: (req, res) =>{
         moduloLogin.validar(req.body.email, req.body.password)
-    .then(resultado=> {
+    .then(results=> {
+        if(results != null){
         DB.Resenas.create({
             texto: req.body.texto,
             puntaje: req.body.puntaje,
-            id_usuario: resultado.id,
+            id_usuario: results.id,
             id_Pelicula: req.params.idPeli
 
-        })
-        .then(function(index){
-            return res.redirect('/peliculas?id='+req.params.idPeli)
-        })
-        .catch(function(error){
-            return res.send(error);
-        }) 
-
-    })
+        });
+        return res.redirect('/peliculas?id='+req.params.idPeli)
+        }else{
+            res.send('Contraseña incorrecta, vuelva para atras');
+        } 
+      })
     },         
 };
